72637bbe8df3270ec174f806506f14c03d0bfb7a
k-akie <kikuchiakie7@gmail.com>
2023-10-27 23:15:51 +0900

    refactor(merge): rename

--- merge_sort.py
+++ merge_sort.py
@@ -4,45 +4,45 @@ import copy
 def merge_sort(t: list[int]):
     target = copy.copy(t)
 
-    tempList = [target[idx:idx + 1] for idx in range(0, len(target), 1)]
+    splitLists = [target[idx:idx + 1] for idx in range(0, len(target), 1)]
     loop = int(len(target) / 2 + 1)
     while loop > 0:
         # 整列済みリスト2組をマージする
         mergedLists = []
-        for i in range(0, int((len(tempList) + 1) / 2)):
+        for i in range(0, int((len(splitLists) + 1) / 2)):
             mergedList = []
 
-            if (2 * i + 1) > len(tempList) - 1:
-                mergedList = tempList[2 * i]
+            if (2 * i + 1) > len(splitLists) - 1:
+                mergedList = splitLists[2 * i]
             else:
-                a = tempList[2 * i]
-                b = tempList[2 * i + 1]
+                list_a = splitLists[2 * i]
+                list_b = splitLists[2 * i + 1]
                 pos_a = 0
                 pos_b = 0
 
-                while pos_a < len(a) or pos_b < len(b):
-                    if pos_a == len(a):
-                        mergedList.append(b[pos_b])
+                while pos_a < len(list_a) or pos_b < len(list_b):
+                    if pos_a == len(list_a):
+                        mergedList.append(list_b[pos_b])
                         pos_b += 1
                         continue
 
-                    if pos_b == len(b):
-                        mergedList.append(a[pos_a])
+                    if pos_b == len(list_b):
+                        mergedList.append(list_a[pos_a])
                         pos_a += 1
                         continue
 
-                    if a[pos_a] > b[pos_b]:
-                        mergedList.append(b[pos_b])
+                    if list_a[pos_a] > list_b[pos_b]:
+                        mergedList.append(list_b[pos_b])
                         pos_b += 1
                         continue
 
-                    mergedList.append(a[pos_a])
+                    mergedList.append(list_a[pos_a])
                     pos_a += 1
 
             mergedLists.append(mergedList)
             print(mergedLists)
 
-        tempList = mergedLists
+        splitLists = mergedLists
         loop -= 1
 
-    return tempList.pop() if tempList else []
+    return splitLists.pop() if splitLists else []
