e87aa902a12e33b1d3dcbb998d2b8ef4a051ed1b
k-akie <kikuchiakie7@gmail.com>
2023-10-27 23:42:11 +0900

    refactor(merge): 補足コメントを追加

--- merge_sort.py
+++ merge_sort.py
@@ -4,11 +4,14 @@ import copy
 def merge_sort(t: list[int]):
     target = copy.copy(t)
 
+    # 最初に要素1個ずつのリストに分割する
     splitLists = [target[idx:idx + 1] for idx in range(0, len(target), 1)]
+    # 2組ずつマージしていくので 最大長 / 2 回だけ繰り返す
     loop = int(len(target) / 2 + 1)
     while loop > 0:
-        # 整列済みリスト2組をマージする
         mergedLists = []
+
+        # 整列済みリスト2組を順にマージする
         for i in range(0, int((len(splitLists) + 1) / 2)):
             # 整列済みリスト2組を選ぶ
             index_a = 2 * i
@@ -17,29 +20,30 @@ def merge_sort(t: list[int]):
             # 整列済みリストが足りなければ空リストで補完
             list_b = [] if index_b > len(splitLists) - 1 else splitLists[index_b]
 
-            # 整列済みリストのうち、どの一の要素を扱っているか
+            # 整列済みリストのうち、どの位置の要素を扱っているか
             pos_a = 0
             pos_b = 0
             mergedList = []
             while pos_a < len(list_a) or pos_b < len(list_b):
+                # 片方のリストが空になっていたら、他方のリストを順に詰める
                 if pos_a == len(list_a):
                     mergedList.append(list_b[pos_b])
                     pos_b += 1
                     continue
-
                 if pos_b == len(list_b):
                     mergedList.append(list_a[pos_a])
                     pos_a += 1
                     continue
 
+                # 両リストの先頭のうち、大きい方を詰める
                 if list_a[pos_a] > list_b[pos_b]:
                     mergedList.append(list_b[pos_b])
                     pos_b += 1
-                    continue
+                else:
+                    mergedList.append(list_a[pos_a])
+                    pos_a += 1
 
-                mergedList.append(list_a[pos_a])
-                pos_a += 1
-
+            # 両リストが空になったら、次に行く
             mergedLists.append(mergedList)
             print(mergedLists)
 
